1b772a0417fca0a6465dba8821e7e5476dfb14ef
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-13 18:47:37 +0200

    add missing 'using'

--- PierbattistaGiacomo/main/ILevel.cs
+++ PierbattistaGiacomo/main/ILevel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Isaccoop.Model.Common;
 
 namespace Isaccoop.Model.Room
 {
--- PierbattistaGiacomo/main/IRoom.cs
+++ PierbattistaGiacomo/main/IRoom.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Isaccoop.Model.Common;
 
 namespace Isaccoop.Model.Room 
 {
