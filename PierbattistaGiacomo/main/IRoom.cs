51cab69426b4ce815dc48c89720b32e773b02de5
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-13 17:43:16 +0200

    complete IRoom.cs

--- PierbattistaGiacomo/main/IRoom.cs
+++ PierbattistaGiacomo/main/IRoom.cs
@@ -42,5 +42,47 @@ namespace Isaccoop.Model.Room
 		///  <returns> the player, or null
 		///  if the <see cref="Player"/>} is not in this room. </returns> 
 		Player GetPlayer();
+
+		/// <summary>
+		///  Get the list of <see cref="Enemy"/>.
+		/// </summary>
+		///  <returns> the list of <see cref="Enemy"/>, or null
+		///  if there are no <see cref="Enemy"/> in this room. </returns>
+		IList<Enemy> GetEnemies();
+
+		/// <summary>
+		/// return width of this room.
+		/// </summary>
+		///  <returns> the width of this room </returns> 
+		int GetWidth();
+
+		/// <summary>
+		/// return height of this room.
+		/// </summary>
+		///  <returns>the height of this room </returns>
+		int GetHeight();
+
+		/// <summary>
+		///  Check if this room is complete.
+		///  A room is complete if all enemies in there are defeated.
+		///  Rooms with neither enemies nor boss are already complete.
+		/// </summary>
+		///  <returns> true if this room is complete, false otherwise </returns>
+		bool IsComplete();
+
+		/// <summary>
+		///  Put the player inside this room. To be used together with <see cref="RemovePlayer"/>
+		///  to move a player from a Room to another.
+		/// </summary>
+		///  <param name="player">the player to be placed in this room.</param>
+		///  <returns> true if the player has been placed successfully in this room, false otherwise </returns>
+		bool AddPlayer(Player player);
+
+		/// <summary>
+		///  Remove the player from this room. To be used together with <see cref="AddPlayer"/>
+		///  to move a player from a Room to another.
+		/// </summary>
+		///  <returns> true if the player has been removed successfully from this room, false otherwise </returns>
+		bool RemovePlayer();
 	}
 }
