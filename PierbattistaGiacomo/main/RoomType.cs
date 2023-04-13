f7fee800c8444d3130f3b2eb02844956a570eb22
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-13 17:57:12 +0200

    add RoomType.cs

---
+++ PierbattistaGiacomo/main/RoomType.cs
@@ -0,0 +1,36 @@
+﻿using Isaccoop.Model.Room;
+
+namespace Isaccoop.Model.Common
+{
+
+	/// <summary>
+	/// This enum lists all possible types of <see cref="IRoom"/> inside a <see cref="ILevel"/>.
+	/// </summary>
+	enum RoomType 
+	{
+		/// <summary>
+		/// An empty room where the player starts.
+		/// </summary>
+		START,
+
+		/// <summary>
+		/// A room with enemies and obstacles.
+		/// </summary>
+		STANDARD,
+
+		/// <summary>
+		/// A room where you can buy power ups.
+		/// </summary>
+		SHOP,
+
+		/// <summary>
+		/// A room with the final boss.
+		/// </summary>
+		BOSS,
+
+		/// <summary>
+		/// A room that contains a free power up.
+		/// </summary>
+		TREASURE
+	}
+}
