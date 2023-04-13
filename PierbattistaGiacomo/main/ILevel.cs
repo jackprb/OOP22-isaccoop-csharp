3ccb241069cf6a5a51374c75353b2878009dadc2
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-13 17:30:04 +0200

    complete ILevel.cs

--- PierbattistaGiacomo/main/ILevel.cs
+++ PierbattistaGiacomo/main/ILevel.cs
@@ -24,5 +24,63 @@ namespace Isaccoop.Model.Room
 		/// </summary>
 		/// <returns>an unmodifiable copy of the list containing all the rooms in this level</returns> 
 		IList<IRoom> GetRooms();
+
+		/// <summary>
+		/// Check if this level is complete.
+		/// A level is complete if all its room are complete.
+		/// </summary>
+		/// <returns>true if this level is complete, false otherwise</returns> 
+		bool IsLevelComplete();
+
+		/// <summary>
+		/// Get the room where the player begins.
+		/// </summary>
+		/// <returns> the room where the player begins</returns>
+		IRoom GetStartRoom();
+
+		/// <summary>
+		/// Get the room where the player currently is.
+		/// </summary>
+		/// <returns> the room where the player currently is</returns>
+		IRoom GetCurrentRoom();
+
+		/// <summary>
+		/// Get a map containing the rooms near the current one and the direction to reach them.
+		/// This map associates a <see cref="IRoom"/> to the <see cref="Direction"/> to take to reach it.
+		/// </summary>
+		/// <returns>a map containing the rooms near the current one and the direction to reach them</returns> 
+		IDictionary<Direction, IRoom> GetNearRooms();
+
+		/// <summary>
+		/// Get the player.
+		/// </summary>
+		/// <returns>the player</returns>
+		Player GetPlayer();
+
+		/// <summary>
+		/// Check if the current room is "completed", that is, if there are no more
+		/// enemies to defeat (STANDARD room), or if the boss has been defeated (BOSS room).
+		/// </summary>
+		/// <returns>always true for NON-STANDARD and NON-BOSS rooms, true if all enemies in STANDARD room
+		/// or the final boss in BOSS room have been defeated, false otherwise</returns> 
+		bool IsCurrentRoomComplete();
+
+		/// <summary>
+		/// Return an umodifiable map of rooms where the player can go, associated with the <see cref="Direction"/>
+		/// to reach them. Player can go to a room that is UP, DOWN, LEFT, RIGHT of current room.
+		/// </summary>
+		/// <returns>an umodifiable map of accessible rooms, near the room where the player is, and the direction
+		/// to reach them</returns> 
+		IDictionary<Direction, IRoom> GetAccessibleRooms();
+
+		/// <summary>
+		/// Move to the previous room.
+		/// </summary>
+		void MoveToPreviousRoom();
+
+		/// <summary>
+		/// Move to the next room.
+		/// </summary>
+		void MoveToNextRoom();
 	}
 }
