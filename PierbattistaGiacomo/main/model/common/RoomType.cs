4ff5ac8db7fe938b05a348f0b1d852c412f63603
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-15 19:49:31 +0200

    improve code style and fix line endings

--- PierbattistaGiacomo/main/model/common/RoomType.cs
+++ PierbattistaGiacomo/main/model/common/RoomType.cs
@@ -2,35 +2,34 @@
 
 namespace Isaccoop.Model.Common
 {
+    /// <summary>
+    /// This enum lists all possible types of <see cref="IRoom"/> inside a <see cref="ILevel"/>.
+    /// </summary>
+    public enum RoomType 
+    {
+        /// <summary>
+        /// An empty room where the player starts.
+        /// </summary>
+        Start,
 
-	/// <summary>
-	/// This enum lists all possible types of <see cref="IRoom"/> inside a <see cref="ILevel"/>.
-	/// </summary>
-	public enum RoomType 
-	{
-		/// <summary>
-		/// An empty room where the player starts.
-		/// </summary>
-		Start,
+        /// <summary>
+        /// A room with enemies and obstacles.
+        /// </summary>
+        Standard,
 
-		/// <summary>
-		/// A room with enemies and obstacles.
-		/// </summary>
-		Standard,
+        /// <summary>
+        /// A room where you can buy power ups.
+        /// </summary>
+        Shop,
 
-		/// <summary>
-		/// A room where you can buy power ups.
-		/// </summary>
-		Shop,
+        /// <summary>
+        /// A room with the final boss.
+        /// </summary>
+        Boss,
 
-		/// <summary>
-		/// A room with the final boss.
-		/// </summary>
-		Boss,
-
-		/// <summary>
-		/// A room that contains a free power up.
-		/// </summary>
-		Treasure
-	}
+        /// <summary>
+        /// A room that contains a free power up.
+        /// </summary>
+        Treasure
+    }
 }
