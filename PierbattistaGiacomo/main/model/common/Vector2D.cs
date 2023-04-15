f5be60b6a773c25370ec670b5aad78919ebe03a7
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-15 19:27:42 +0200

    add Vector2D.cs

---
+++ PierbattistaGiacomo/main/model/common/Vector2D.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isaccoop.Model.Common {
+	/// <summary>
+	/// Class to model a vector into 2d space.
+	/// </summary>
+	public class Vector2D {
+
+		public double X { get; }
+		public double Y { get; }
+
+		/// <summary>
+		/// Constructor for <see cref="Vector2D"/>.
+		/// </summary>
+		/// <param name = "x"> x coordinate</param> 
+		/// <param name = "y"> y coordinate</param>
+		public Vector2D(double x, double y) {
+			X = x;
+			Y = y;
+		}
+
+		/// <summary>
+		/// Constructor for <see cref="Vector2D"/> using the difference between two <see cref="Point2D"/>.
+		/// </summary>
+		/// <param name="to">destination point</param>
+		/// <param name="from">origin point</param>
+		public Vector2D(Point2D to, Point2D from) {
+			X = to.getX() - from.getX();
+			Y = to.getY() - from.getY();
+		}
+
+		/// <summary>
+		/// Method to sum two <see cref="Vector2D"/>.
+		/// </summary>
+		/// <param name="v">vector to sum</param> v 
+		/// <returns>sum between two <see cref="Vector2D"/></returns>
+		public Vector2D Sum(Vector2D v) => new Vector2D(X + v.x, Y + v.y);
+
+		/// <summary>
+		/// Method to compute the module of this <see cref="Vector2D"/>.
+		/// </summary>
+		/// <returns>module of this vector</returns> 
+		public double Module() => Math.Sqrt(X * X + Y * Y);
+
+		/// <summary>
+		/// Method to normalize this <see cref="Vector2D"/>.
+		/// </summary>
+		/// <returns> normalized vector</returns>
+		public Vector2D GetNormalized() {
+			double module = Module();
+			return new Vector2D(X / module, Y / module);
+		}
+
+		/// <summary>
+		/// Method for multiplying the vector by a scalar number.
+		/// </summary>
+		/// <param name="fact">scalar number</param> 
+		/// <returns> computed vector result</returns>
+		public Vector2D Mul(double fact) => new Vector2D(X * fact, Y * fact);
+	}
+}
