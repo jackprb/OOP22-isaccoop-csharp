e8f57f419246767c504d6e26a3f3849d461faaeb
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-04-15 18:31:32 +0200

    Added Spawn Interface

---
+++ DottiAndrea/main/model/spawn/ISpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using Isaccoop.Model.Common;
+
+namespace Isaccoop.Model.Spawn
+{
+    /// <summary>
+    /// Spawn interface to model entities delegates to spawn other entities.
+    /// </summary>
+    public interface ISpawn
+    {
+        /// <summary>
+        /// Method that sets the positions of the <see cref="MapElement"/> inside the room.
+        /// </summary>
+        /// <param name="elementsToSpawn"></param>
+        /// <param name="width"> of room</param>
+        /// <param name="height"> of room</param>
+        void SetPosition(List<MapElement> elementsToSpawn, int width, int height);
+    }
+}
