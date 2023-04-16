8a9caa717405633a10074d2bcbe52a896b9f67d4
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-04-16 13:13:11 +0200

    Modified TestBoundingBox

--- DottiAndrea/main/model/boundingbox/TestBoundingBox.cs
+++ DottiAndrea/main/model/boundingbox/TestBoundingBox.cs
@@ -55,41 +55,5 @@ namespace it.unibo.isaccoop.test
             Assert.AreEqual(10, width);
             Assert.AreEqual(20, height);
         }
-
-        [Test]
-        public void TestRectBoundingBoxIsCollidingWithCricle()
-        {
-            // Arrange
-            var rectangleBox = new RectBoundingBox(10, 20);
-            var center1 = new Point2D(0, 0);
-            var center2 = new Point2D(15, 25);
-            var circleBox = new CircleBoundingBox(5);
-
-            // Act
-            var result1 = rectangleBox.isCollidingWithCricle(center1, center2, circleBox);
-            var result2 = rectangleBox.isCollidingWithCricle(center1, center2, new CircleBoundingBox(10));
-
-            // Assert
-            Assert.IsTrue(result1);
-            Assert.IsFalse(result2);
-        }
-
-        [Test]
-        public void TestRectBoundingBoxIsCollidingWithRecPerimeter()
-        {
-            // Arrange
-            var rectangleBox1 = new RectBoundingBox(10, 20);
-            var rectangleBox2 = new RectBoundingBox(5, 5);
-            var center1 = new Point2D(5, 5);
-            var center2 = new Point2D(15, 25);
-
-            // Act
-            var result1 = rectangleBox1.isCollidingWithRecPerimeter(center1, rectangleBox2);
-            var result2 = rectangleBox1.isCollidingWithRecPerimeter(center2, rectangleBox2);
-
-            // Assert
-            Assert.IsTrue(result1);
-            Assert.IsFalse(result2);
-        }
     }
 }
