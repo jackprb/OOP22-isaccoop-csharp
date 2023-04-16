ddc27f7292e0787855b35995b115d2e342a3467e
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-04-16 12:18:25 +0200

    Added skeleton of TestSpawn

---
+++ DottiAndrea/main/model/spawn/TestSpawn.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTool.UnitTesting;
+using Isaccoop.Model.Common;
+using Isaccoop.Model.Spawn;
+using System.Collections.Generic;
+
+namespace Isaccoop.Model.Tests.Spawn
+{
+    [TestFixture]
+    public class SpawnOrderedTests
+    {
+        [Test]
+        public void SetPosition_OneElement_CenterPosition()
+        {
+            /// Arrange
+            var spawn = new SpawnOrdered();
+            var elements = new List<MapElement> { new MapElement() };
+            int width = 10;
+            int height = 10;
+
+            /// Act
+            spawn.SetPosition(elements, width, height);
+
+            /// Assert
+            Assert.AreEqual(new Point2D(width / 2.0, height / 2.0), elements[0].Coords);
+        }
+
+        [Test]
+        public void SetPosition_MultipleElements_OrderedYPositions()
+        {
+            /// Arrange
+            var spawn = new SpawnOrdered();
+            var elements = new List<MapElement>
+            {
+                new MapElement(),
+                new MapElement(),
+                new MapElement()
+            };
+            int width = 10;
+            int height = 10;
+
+            /// Act
+            spawn.SetPosition(elements, width, height);
+
+            /// Assert
+            Assert.AreEqual(new Point2D(width / 2.0, height / 4.0), elements[0].Coords);
+            Assert.AreEqual(new Point2D(width / 2.0, height / 2.0), elements[1].Coords);
+            Assert.AreEqual(new Point2D(width / 2.0, 3 * height / 4.0), elements[2].Coords);
+        }
+    }
+}
