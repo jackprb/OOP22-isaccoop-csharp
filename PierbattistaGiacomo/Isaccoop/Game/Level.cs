91be29bbc4dc62c65fa4c7104e224797431fbb76
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-17 10:25:13 +0200

    add Level.cs

---
+++ PierbattistaGiacomo/Isaccoop/Game/Level.cs
@@ -0,0 +1,38 @@
+﻿using Isaccoop.Game.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Isaccoop.Game 
+{
+    /// <summary>
+    /// Implementation of <see cref="ILevel"/>
+    /// </summary>
+    public class Level : ILevel
+    {
+        private readonly List<IRoom> _rooms = new List<IRoom>();
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns><inheritdoc/></returns>
+        public void PutRooms(IList<IRoom> roomList)
+        {
+            if (!roomList.Any() || roomList.Count < Enum.GetValues(typeof(RoomType)).Length)
+            {
+                throw new ArgumentException("This level must have at least " + Enum.GetValues(typeof(RoomType)).Length
+                        + " rooms");
+            }
+            if (!_rooms.Any())
+            {
+                _rooms.AddRange(roomList);
+            }
+            else
+            {
+                throw new InvalidOperationException("This level already has a room list");
+            }
+        }
+
+        
+    }
+}
