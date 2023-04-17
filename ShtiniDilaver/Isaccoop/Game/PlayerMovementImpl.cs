f307cb26a0a1ecb2492683ffe4279337618d58d6
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2023-04-17 17:27:53 +0200

    create PlayerMovementImpl

---
+++ ShtiniDilaver/Isaccoop/Game/PlayerMovementImpl.cs
@@ -0,0 +1,11 @@
+namespace Isaccoop.Game{
+
+    /// <summary>
+    /// The class for player movement.
+    /// </summary>
+    public class PlayerMovementImpl : IPlayerMovement
+    {
+        
+    }
+
+}
