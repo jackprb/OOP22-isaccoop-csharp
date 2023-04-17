1009cbe0051e6665c305425393982d1ff69eadac
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-17 12:52:49 +0200

    remove methods involving unimplemented parts

--- PierbattistaGiacomo/Isaccoop/Game/IRoom.cs
+++ PierbattistaGiacomo/Isaccoop/Game/IRoom.cs
@@ -15,41 +15,6 @@ namespace Isaccoop.Game
         public RoomType GetRoomType();
 
         /// <summary>
-        ///  Get the <see cref="AIEnemy"/> attached to this <see cref="IRoom"/>.
-        ///  </summary>
-        ///  <returns> <see cref="AIEnemy"/> attached to this <see cref="IRoom"/>, or null
-        ///  if there is not an <see cref="AIEnemy"/> in this room </returns> 
-        public AIEnemy GetRoomAI();
-
-        /// <summary>
-        ///  Get the list of <see cref="Item"/>s in this room.
-        /// </summary>
-        ///  <returns> the list of items in this room, or null
-        ///  if there is not a list of <see cref="Item"/>s in this room. </returns>
-        public IList<Item> GetItems();
-
-        /// <summary>
-        ///  Get the list of <see cref="PowerUp"/> in this room.
-        /// </summary>
-        ///  <returns>the list of <see cref="PowerUp"/>s in this room, or null
-        ///  if there is not a list of <see cref="PowerUp"/>s in this room. </returns> 
-        public IList<PowerUp> GetPowerUps();
-
-        /// <summary>
-        ///  Get the <see cref="Player"/>.
-        /// </summary>
-        ///  <returns> the player, or null
-        ///  if the <see cref="Player"/>} is not in this room. </returns> 
-        public Player GetPlayer();
-
-        /// <summary>
-        ///  Get the list of <see cref="Enemy"/>.
-        /// </summary>
-        ///  <returns> the list of <see cref="Enemy"/>, or null
-        ///  if there are no <see cref="Enemy"/> in this room. </returns>
-        public IList<Enemy> GetEnemies();
-
-        /// <summary>
         /// return width of this room.
         /// </summary>
         ///  <returns> the width of this room </returns> 
@@ -60,28 +25,5 @@ namespace Isaccoop.Game
         /// </summary>
         ///  <returns>the height of this room </returns>
         public int GetHeight();
-
-        /// <summary>
-        ///  Check if this room is complete.
-        ///  A room is complete if all enemies in there are defeated.
-        ///  Rooms with neither enemies nor boss are already complete.
-        /// </summary>
-        ///  <returns> true if this room is complete, false otherwise </returns>
-        public bool IsComplete();
-
-        /// <summary>
-        ///  Put the player inside this room. To be used together with <see cref="RemovePlayer"/>
-        ///  to move a player from a Room to another.
-        /// </summary>
-        ///  <param name="player">the player to be placed in this room.</param>
-        ///  <returns> true if the player has been placed successfully in this room, false otherwise </returns>
-        public bool AddPlayer(Player player);
-
-        /// <summary>
-        ///  Remove the player from this room. To be used together with <see cref="AddPlayer"/>
-        ///  to move a player from a Room to another.
-        /// </summary>
-        ///  <returns> true if the player has been removed successfully from this room, false otherwise </returns>
-        public bool RemovePlayer();
     }
 }
--- PierbattistaGiacomo/Isaccoop/Game/Room.cs
+++ PierbattistaGiacomo/Isaccoop/Game/Room.cs
@@ -1,17 +1,13 @@
 ﻿using Isaccoop.Game.Common;
-using System.Collections.Generic;
 
-namespace Isaccoop.Game {
+namespace Isaccoop.Game
+{
     /// <summary>
     /// Implementation of <see cref="IRoom"/>.
     /// </summary>
-    public class Room : IRoom {
-
+    public class Room : IRoom
+    {
         private readonly RoomType _roomType;
-        private readonly AIEnemy? _roomAi;
-        private readonly IList<Item>? _items;
-        private readonly IList<PowerUp>? _powerups;
-        private Player? _player;
         private readonly int _width;
         private readonly int _height;
 
@@ -20,21 +16,10 @@ namespace Isaccoop.Game {
         /// </summary>
         /// <param name="width"> horizontal dimension of this room</param> 
         /// <param name="height"> vertical dimension of this room</param>
-        /// <param name="coord"> coordinates of this room inside the level</param> 
         /// <param name="roomType"> type of this room</param> 
-        /// <param name="roomAI"> the AiEnemy for this room</param> 
-        /// <param name="items">the items in this room</param> 
-        /// <param name="powerups">the powerups in this room</param> 
-        /// <param name="player">the player</param> 
-        public Room(int width, int height, Point2D coord, RoomType roomType,
-                AIEnemy? roomAI, IList<Item>? items, IList<PowerUp>? powerups, Player? player) {
-            //super(width, height);
-            //super.setCoords(coord);
+        public Room(int width, int height, RoomType roomType)
+        {
             _roomType = roomType;
-            _roomAi = roomAI;
-            _items = items;
-            _powerups = powerups;
-            _player = player;
             _width = width;
             _height = height;
         }
@@ -49,41 +34,6 @@ namespace Isaccoop.Game {
         /// <inheritdoc/>
         /// </summary>
         /// <returns><inheritdoc/></returns>
-        public AIEnemy GetRoomAI() => _roomAi;
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
-        public IList<Item> GetItems() => _items;
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
-        public IList<PowerUp> GetPowerUps() => _powerups;
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
-        public Player GetPlayer() => _player;
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
-        public IList<Enemy> GetEnemies() {
-            if (_roomAi.HasValue) {
-                return _roomAi.GetValueOrDefault();
-            }
-            return _roomAi.Value.getControlledEnemies();
-        }
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
         public int GetWidth() => _width;
 
         /// <summary>
@@ -91,23 +41,5 @@ namespace Isaccoop.Game {
         /// </summary>
         /// <returns><inheritdoc/></returns>
         public int GetHeight() => _height;
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
-        public bool IsComplete() => false;
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
-        public bool AddPlayer(Player player) => false;
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        /// <returns><inheritdoc/></returns>
-        public bool RemovePlayer() => false;
     }
 }
