5a81d7c0eefebf35c18a3efe40eddff80c517b98
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-17 17:55:18 +0200

    complete implementation of LevelTest.cs

--- PierbattistaGiacomo/Isaccoop/Test/LevelTest.cs
+++ PierbattistaGiacomo/Isaccoop/Test/LevelTest.cs
@@ -28,6 +28,38 @@ namespace Isaccoop.Game.Test {
             _localLevel.PutRooms(_roomListFromFactory); // the level has no rooms => OK
         }
 
-        
+        [TestMethod]
+        public void TestPutRooms()
+        {
+            var arr = Enum.GetValues(typeof(RoomType));
+            // generate another List<Room>, made of 5 rooms, one for each RoomType
+            foreach (RoomType roomType in arr)
+            {
+                _otherRoomList.Add(new RoomFactory(arr.Length)
+                        .BuildRoomInProperOrder(new Point2D(((int) roomType), 0)));
+            }
+
+            // the level already has rooms (set in method setUp()) => throw exception
+            Assert.ThrowsException<InvalidOperationException>(() => _localLevel.PutRooms(_otherRoomList));
+
+            // the localLevel must have the correct number of rooms
+            Assert.Equals(_localLevel.GetRooms().Count, _roomListFromFactory.Count);
+        }
+
+        [TestMethod]
+        public void TestGetRooms()
+        {
+            // the List<Room> returned from localLevel is the same in roomListFromFactory
+            // so, those lists must be equal
+            Assert.Equals(_localLevel.GetRooms(), _roomListFromFactory);
+        }
+
+        [TestMethod]
+        public void TestGetStartRoom()
+        {
+            IRoom startRoom = _localLevel.GetRooms()
+                   .Find(r => r.GetRoomType() == RoomType.Start);
+            Assert.Equals(_localLevel.GetStartRoom().GetRoomType(), startRoom.GetRoomType());
+        }
     }
 }
