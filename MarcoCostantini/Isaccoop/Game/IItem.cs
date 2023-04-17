8ee8b5b761ccc21aee9977f8eb4aa6fb375ed453
MarcoCostantini26 <marco.costantini26@gmail.com>
2023-04-17 14:03:19 +0200

    add item interface and abstract class

---
+++ MarcoCostantini/Isaccoop/Game/IItem.cs
@@ -0,0 +1,12 @@
+namespace Isaccoop.Game;
+
+/// <summary>
+/// Interface that models items.
+/// </summary>
+public interface IItem
+{
+    /// <summary>
+    /// Method for interacting with player stats.
+    /// </summary>
+    void Interact();
+}
