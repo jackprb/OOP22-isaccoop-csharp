92539dbbbe920b61608efaf8b6176ad66b763daf
Giacomo Pierbattista <giacomo.pierbattista@studio.unibo.it>
2023-04-17 10:31:03 +0200

    add ILevelFactory.cs

---
+++ PierbattistaGiacomo/Isaccoop/Game/ILevelFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Isaccoop.Game
+{
+    /// <summary>
+    /// Interface that allows to create a <see cref="ILevel"/> dynamically.
+    /// </summary>
+    public interface ILevelFactory
+    {
+        /// <summary>
+        /// Generates dynamically a <see cref="ILevel"/> made of a random number of <see cref="IRoom"/>s.
+        /// The level will have at least 6 rooms and at most 30.
+        /// </summary>
+        ///<returns>the level created</returns>
+        public ILevel CreateLevel();
+    }
+}
