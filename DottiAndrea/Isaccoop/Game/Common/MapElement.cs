531a74ac4880c7f64c417534634889364a6ff4f7
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-04-18 09:57:04 +0200

    Fixed MapElement

--- DottiAndrea/Isaccoop/Game/Common/MapElement.cs
+++ DottiAndrea/Isaccoop/Game/Common/MapElement.cs
@@ -3,70 +3,70 @@
 namespace Isaccoop.Game.Common
 {
     public class MapElement : IMapElement
-{
-    private Point2D coords;
-    private IBoundingBox box;
-    private const double FIXED_INITIAL_POSITION = 10.0;
-
-    public enum ElementsRadius
     {
-        DEFAULT = 0,
-        ENEMY = 8,
-        ITEM = 5,
-        PLAYER = 8,
-        BOSS = 20,
-        BULLET = 3
-    }
+        private readonly Point2D _coords;
+        private readonly IBoundingBox _box;
+        private const double FIXED_INITIAL_POSITION = 10.0;
 
-    public MapElement(ElementsRadius elemRadius)
-    {
-        this.coords = new Point2D(FIXED_INITIAL_POSITION, FIXED_INITIAL_POSITION);
-        this.box = new CircleBoundingBox((double) elemRadius);
-    }
-
-    public MapElement(int width, int height)
-    {
-        this.coords = new Point2D(0.0, 0.0);
-        this.box = new RectBoundingBox(width, height);
-    }
-
-    public Point2D GetCoords()
-    {
-        return new Point2D(this.coords.X, this.coords.Y);
-    }
-
-    public void SetCoords(Point2D coords)
-    {
-        this.coords = new Point2D(coords.X, coords.Y);
-    }
-
-    public IBoundingBox GetBox()
-    {
-        return this.box;
-    }
-
-    public override int GetHashCode()
-    {
-        return coords.GetHashCode();
-    }
-
-    public override bool Equals(object obj)
-    {
-        if (this == obj)
+        public enum ElementsRadius
         {
-            return true;
+            DEFAULT = 0,
+            ENEMY = 8,
+            ITEM = 5,
+            PLAYER = 8,
+            BOSS = 20,
+            BULLET = 3
         }
-        if (obj == null)
+
+        public MapElement(ElementsRadius elemRadius)
         {
-            return false;
+            _coords = new Point2D(FIXED_INITIAL_POSITION, FIXED_INITIAL_POSITION);
+            _box = new CircleBoundingBox((double) elemRadius);
         }
-        if (GetType() != obj.GetType())
+
+        public MapElement(int width, int height)
         {
-            return false;
+            _coords = new Point2D(0.0, 0.0);
+            _box = new RectBoundingBox(width, height);
         }
-        MapElement other = (MapElement)obj;
-        return coords.X.CompareTo(other.coords.X) == 0
-            && coords.Y.CompareTo(other.coords.Y) == 0;
+
+        public Point2D GetCoords()
+        {
+            return new Point2D(_coords.X, _coords.Y);
+        }
+
+        public void SetCoords(Point2D coords)
+        {
+            coords = new Point2D(coords.X, coords.Y);
+        }
+
+        public IBoundingBox GetBox()
+        {
+            return _box;
+        }
+
+        public override int GetHashCode()
+        {
+            return _coords.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+            if (obj == null)
+            {
+                return false;
+            }
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+            MapElement other = (MapElement)obj;
+            return _coords.X.CompareTo(other._coords.X) == 0
+                && _coords.Y.CompareTo(other._coords.Y) == 0;
+        }
     }
-}
 }
