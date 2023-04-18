a7040408b39aa12c8930313e956588d1081acdde
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-04-18 10:40:57 +0200

    Add IRemovable interface and ConcreteAiEnemy class

---
+++ DanielePancottini/Isaccoop/Game/Common/IRemovable.cs
@@ -0,0 +1,10 @@
+﻿namespace Isaccoop.Game.Common;
+
+public interface IRemovable
+{
+    /// <summary>
+    /// Remove a certain element.
+    /// </summary>
+    /// <param name="e"></param>
+    void Remove(IMapElement e);
+}
