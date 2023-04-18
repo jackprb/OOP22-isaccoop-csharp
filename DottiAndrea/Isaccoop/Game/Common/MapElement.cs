bc0270b02acdad5266479770c6f5f34234ad8cc8
Andrea Dotti <andrea.dotti4@studio.unibo.it>
2023-04-18 11:48:18 +0200

    Added documentation to MapElement

--- DottiAndrea/Isaccoop/Game/Common/MapElement.cs
+++ DottiAndrea/Isaccoop/Game/Common/MapElement.cs
@@ -2,12 +2,18 @@
 
 namespace Isaccoop.Game.Common
 {
+    /// <summary>
+    /// Class that implements IMapElement interface.
+    /// </summary>
     public class MapElement : IMapElement
     {
         private Point2D _coords;
         private readonly IBoundingBox _box;
         private const double FIXED_INITIAL_POSITION = 10.0;
 
+        /// <summary>
+        /// Enum for elements radius.
+        /// </summary>
         public enum ElementsRadius
         {
             DEFAULT = 0,
@@ -18,38 +24,54 @@ namespace Isaccoop.Game.Common
             BULLET = 3
         }
 
+        /// <summary>
+        /// Class that implements IMapElement interface
+        /// <param name="elemRadius"> of circle boundingBox</param>
+        /// </summary>
         public MapElement(ElementsRadius elemRadius)
         {
             _coords = new Point2D(FIXED_INITIAL_POSITION, FIXED_INITIAL_POSITION);
             _box = new CircleBoundingBox((double) elemRadius);
         }
 
+        /// <summary>
+        /// Class that implements IMapElement interface
+        /// <param name="width"> of the rectangle boundingBox</param>
+        /// <param name="height"> of the rectangle boundingBox</param>
+        /// </summary>
         public MapElement(int width, int height)
         {
             _coords = new Point2D(0.0, 0.0);
             _box = new RectBoundingBox(width, height);
         }
 
+        /// <returns> The current MapElement position</returns>
         public Point2D GetCoords()
         {
             return new Point2D(_coords.X, _coords.Y);
         }
 
+        /// <summary>
+        /// Set the new Coords to the mapElement.
+        /// </summary>
         public void SetCoords(Point2D coords)
         {
             _coords = new Point2D(coords.X, coords.Y);
         }
 
+        /// <returns> The boundingBox of the mapElement</returns>
         public IBoundingBox GetBox()
         {
             return _box;
         }
 
+        /// <returns> HashCode of the Coords</returns>
         public override int GetHashCode()
         {
             return _coords.GetHashCode();
         }
 
+        /// <returns> True if two obj are equals</returns>
         public override bool Equals(object obj)
         {
             if (this == obj)
