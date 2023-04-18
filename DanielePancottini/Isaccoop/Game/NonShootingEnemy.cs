452267fce44da6bbe8dde4fc4bf7eff7971d8a54
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-04-18 11:38:04 +0200

    Add IHitStrategy and IMovementStrategy blank interfaces, add AbstractEnemy and NonShootingEnemy classes

---
+++ DanielePancottini/Isaccoop/Game/NonShootingEnemy.cs
@@ -0,0 +1,13 @@
+﻿using Isaccoop.Game.Common;
+
+namespace Isaccoop.Game;
+
+public class NonShootingEnemy : AbstractEnemy
+{
+    
+    public NonShootingEnemy() : base(EnemyHearts.BaseEnemyHearts, new NonShootingMovementStrategy(),
+        new NonShootingHitStrategy())
+    {
+        //
+    }
+}
---
+++ DanielePancottini/Isaccoop/Game/NonShootingMovementStrategy.cs
@@ -0,0 +1,11 @@
+﻿using Isaccoop.Game.Common;
+
+namespace Isaccoop.Game;
+
+public class NonShootingMovementStrategy : IMovementStrategy
+{
+    public Point2D Move(Point2D enemyPosition, Point2D playerPosition)
+    {
+        throw new NotImplementedException();
+    }
+}
