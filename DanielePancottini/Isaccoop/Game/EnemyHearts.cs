4c45906493bbda3c8015457e96dcfe37a8412253
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-04-19 19:30:40 +0200

    Add doc to classes and interfaces

--- DanielePancottini/Isaccoop/Game/EnemyHearts.cs
+++ DanielePancottini/Isaccoop/Game/EnemyHearts.cs
@@ -1,5 +1,8 @@
 ﻿namespace Isaccoop.Game;
 
+/// <summary>
+/// Struct containing enemy max hearts configurations.
+/// </summary>
 public struct EnemyHearts
 {
     public const double BaseEnemyHearts = 3.0;
