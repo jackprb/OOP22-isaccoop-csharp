58bbbb030baf6bfbdd9583e01ff3cb6f6c3a9a52
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-04-19 20:03:26 +0200

    Add AiEnemyTest class

---
+++ DanielePancottini/Isaccoop/Test/AiEnemyTest.cs
@@ -0,0 +1,36 @@
+﻿
+using Isaccoop.Game;
+using NUnit.Framework;
+
+namespace Isaccoop.Test;
+
+public class AiEnemyTest
+{
+    
+    private const int MaxEnemies = 5;
+
+    [Test]
+    public void EnemiesCreation()
+    {
+        Assert.AreEqual(MaxEnemies, GetNEnemies(MaxEnemies).Count);
+    }
+
+    
+    [Test]
+    public void AiEnemyCreation()
+    {
+        Assert.NotNull(new ConcreteAiEnemy(GetNEnemies(MaxEnemies)));
+    }
+
+    [Test]
+    public void RemoveAllEnemies()
+    {
+        var enemies = GetNEnemies(MaxEnemies);
+        var ai = new ConcreteAiEnemy(enemies);
+        enemies.ToList().ForEach(enemy => ai.Remove(enemy));
+        Assert.AreEqual(0, ai.GetControlledEnemies().Count);
+    }
+
+    private IList<NonShootingEnemy> GetNEnemies(int maxEnemies) => Enumerable.Range(1, maxEnemies).Select(_ => new NonShootingEnemy()).ToList();
+
+}
