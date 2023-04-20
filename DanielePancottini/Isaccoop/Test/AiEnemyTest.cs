1a1266036a9bc4a5fae52df243ccfe0712f13b17
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-04-20 19:57:12 +0200

    Refactor AiEnemyTest class

--- DanielePancottini/Isaccoop/Test/AiEnemyTest.cs
+++ DanielePancottini/Isaccoop/Test/AiEnemyTest.cs
@@ -9,36 +9,28 @@ namespace Isaccoop.Test;
 /// </summary>
 public class AiEnemyTest
 {
-    
+
     private const int MaxEnemies = 5;
 
     /// <summary>
     /// Check if enemies are created without errors.
     /// </summary>
     [Test]
-    public void EnemiesCreation()
-    {
-        Assert.AreEqual(MaxEnemies, GetNEnemies(MaxEnemies).Count);
-    }
-
+    public void CreateEnemies() => Assert.AreEqual(MaxEnemies, GetNEnemies(MaxEnemies).Count);
 
     /// <summary>
     /// Check if ConcreteAiEnemy instance is created without errors.
     /// </summary>
     [Test]
-    public void AiEnemyCreation()
-    {
-        Assert.NotNull(new ConcreteAiEnemy(GetNEnemies(MaxEnemies)));
-    }
+    public void CreateAiEnemy() => Assert.NotNull(new ConcreteAiEnemy(GetNEnemies(MaxEnemies)));
 
-    /// <summary>
+        /// <summary>
     /// Check if the AI method to get controlled enemies works properly
     /// </summary>
     [Test]
     public void GetAiEnemies()
     {
-        var enemies = GetNEnemies(MaxEnemies);
-        var ai = new ConcreteAiEnemy(enemies);
+        var ai = GetAiWithEnemies(MaxEnemies);
         Assert.AreEqual(MaxEnemies, ai.GetControlledEnemies().Count);
     }
 
@@ -46,18 +38,24 @@ public class AiEnemyTest
     /// Check if the remove all enemies stored into an AI goes without errors.
     /// </summary>
     [Test]
-    public void RemoveAllEnemies()
+    public void RemoveAllAiEnemies()
     {
-        var enemies = GetNEnemies(MaxEnemies);
-        var ai = new ConcreteAiEnemy(enemies);
-        enemies.ToList().ForEach(enemy => ai.Remove(enemy));
+        var ai = GetAiWithEnemies(MaxEnemies);
+        ai.GetControlledEnemies().ForEach(enemy => ai.Remove(enemy));
         Assert.AreEqual(0, ai.GetControlledEnemies().Count);
     }
 
     /// <summary>
+    /// Get a new ai enemy populated with "maxEnemies" enemies.
+    /// </summary>
+    /// <param name="maxEnemies">number of enemies to attach to AI</param>
+    /// <returns></returns>
+    private IAiEnemy GetAiWithEnemies(int maxEnemies) => new ConcreteAiEnemy(GetNEnemies(maxEnemies));
+
+    /// <summary>
     /// Create a NonShootingEnemy collection with N elements.
     /// </summary>
-    /// <param name="maxEnemies">number of elements to create</param>
+    /// <param name="maxEnemies">number of enemies to create</param>
     /// <returns>NonShootingEnemy collection populated with 'maxEnemies' elements</returns>
     private IList<NonShootingEnemy> GetNEnemies(int maxEnemies) => Enumerable.Range(1, maxEnemies).Select(_ => new NonShootingEnemy()).ToList();
 
